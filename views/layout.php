45dab4f4235a7471823fe20761ccbc5ba027a2b5
Colin <cosy96@hotmail.com>
2015-11-09 11:01:20 +0100

    Added HTML and Bootstrap to Layout

--- views/layout.php
+++ views/layout.php
@@ -1,1 +1,15 @@
-
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Blog</title>
+    <link href="/Blog/public/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <?php echo $body_content; ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/Blog/public/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
