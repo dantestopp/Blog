b321bfa317677273764393374065f16408e7d877
happyoniens <dante.stoppini@gmail.com>
2015-11-09 11:14:08 +0100

    Changed User Model

--- model/user.php
+++ model/user.php
@@ -1,5 +1,23 @@
 <?php
 
 class user{
-  
+  public $id;
+  public $prename;
+  public $surname;
+  public $bio;
+  public $email;
+
+  function __construct($arr){
+    $this->id = $arr['id'];
+    $this->prename = $arr['prename'];
+    $this->surname = $arr['surname'];
+    $this->bio = $arr['bio'];
+    $this->email = $arr['email'];
+  }
+  /**
+  * Function to store a new User
+  */
+  public function store(){
+
+  }
 }
