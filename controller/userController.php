67bfc8e4089ca965451fa1071e9b29412f91c082
happyoniens <dante.stoppini@gmail.com>
2015-11-16 08:33:10 +0100

    Added GetUserWithId Function

--- controller/userController.php
+++ controller/userController.php
@@ -9,4 +9,17 @@
 
 class userController{
 
+    /**
+    * Get User with Id
+    * @param Int Id of User
+    * @return User
+    */
+    public static function getUserWithId($id){
+      $sql = "SELECT * FROM user WHERE id = '$id'";
+      $result = Flight::db()->query($sql);
+      if($result != false){
+        return new user($result->fetch_assoc());
+      }
+    }
+
 }
