2e5be63c413470ad43b188c9249ff1c532b125e4
happyoniens <dante.stoppini@gmail.com>
2015-11-16 11:17:14 +0100

    Added Routes for Post creation and update

--- conf/routes.php
+++ conf/routes.php
@@ -1,7 +1,15 @@
 <?php
 Flight::route('GET /',['viewController','index']);
 
-Flight::route('GET /post/@post_id',['viewController','post']);
+Flight::route('GET /post/@post_id',['viewController','viewPost']);
+
+Flight::route('GET /create',['viewController','createPost']);
+
+Flight::route('POST /create',['postController','createPost']);
+
+Flight::route('GET /edit/@id',['viewController','editPost']);
+
+Flight::route('POST /edit',['postController','editPost']);
 
 Flight::route('GET /login',['viewController','login']);
 
--- controller/viewController.php
+++ controller/viewController.php
@@ -15,12 +15,28 @@ class viewController{
     Flight::util()->render("index",["posts"=>$posts]);
   }
 
-  public static function post($id){
+  public static function viewPost($id){
     $posts = new postController();
     $post = $posts->getPostWithId($id);
     Flight::util()->render("post",["post"=>$post]);
   }
 
+  public static function createPost(){
+    if(Flight::has('currentUser')){
+      Flight::util()->render('postEditor');
+    }else{
+      Flight::redirect('/');
+    }
+  }
+
+  public static function editPost($id){
+    if(Flight::has('currentUser')){
+      $post = Flight::posts()->getPostWithId($id);
+      Flight::util()->render('postEditor',['post'=>$post]);
+    }else{
+      Flight::redirect('/');
+    }
+  }
   public static function login(){
     Flight::util()->render("login");
   }
