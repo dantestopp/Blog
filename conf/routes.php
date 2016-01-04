cc78d74def02e19bba651b6ae973c20110b9c28d
happyoniens <dante.stoppini@gmail.com>
2016-01-04 11:09:42 +0100

    Removed Post editing

--- conf/routes.php
+++ conf/routes.php
@@ -7,10 +7,6 @@ Flight::route('GET /create',['viewController','createPost']);
 
 Flight::route('POST /create',['postController','createPost']);
 
-Flight::route('GET /edit/@id',['viewController','editPost']);
-
-Flight::route('POST /edit',['postController','editPost']);
-
 Flight::route('GET /login',['viewController','login']);
 
 Flight::route('POST /login',['authController','login']);
