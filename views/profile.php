a15564cf514566d6c3b3165742715d26a347b191
Colin <cosy96@hotmail.com>
2016-01-11 09:07:17 +0100

    Improved Authors page
    Improved Authors page

--- views/profile.php
+++ views/profile.php
@@ -1,2 +1,5 @@
 <h1>Author</h1>
-<h3>Name: <?php echo $user->fullName(); ?>
+<h3>Name: <?php echo $user->fullName(); ?></h3><br>
+<p><b>Bio:</b> <?php echo $user->bio; ?><br><br>
+<a href="mailto:<?php echo $user->email; ?>"><?php echo $user->email; ?></a><br>
+</p>
